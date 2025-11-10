c80cc3bdd4f6b7c5c45b7c00eec0a0f5789f8147
Cinthya <cinthya45253@gmail.com>
2025-11-09 20:27:19 -0700

    add footer and change icon

--- src/pages/footer/footer.template.tsx
+++ src/pages/footer/footer.template.tsx
@@ -1,20 +1,22 @@
-import { Box, Text } from "@chakra-ui/react"
+import { Box, Text } from "@chakra-ui/react";
 
 export const FooterComponent = () => {
-    return (
-        <Box bottom={0} display="flex"
-            flexDirection="row"
-            justifyContent="space-between"
-            alignItems="center"
-            px={3}
-            boxShadow="0px 2px 8px rgba(0, 0, 0, 0.15)"
-            borderBottom="1px solid #ddd"
-            bgColor="background.paper"
-            // zIndex={10}
-            w="100%"
-            h={20}
-            >
-            <Text>Aqui va el footer jeje</Text>
-        </Box>
-    )
-}
+  return (
+    <Box
+      bottom={0}
+      display="flex"
+      flexDirection="row"
+      justifyContent="space-between"
+      alignItems="center"
+      px={3}
+    //   boxShadow="0px 0px 8px rgba(0, 0, 0, 0.15)"
+      borderTop="1px solid #ddd"
+      bgColor="background.paper"
+      zIndex={10}
+      w="100%"
+      h={20}
+    >
+      <Text>Aqui va el footer jeje</Text>
+    </Box>
+  );
+};
